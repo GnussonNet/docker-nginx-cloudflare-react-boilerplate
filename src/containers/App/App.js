c0c5514886919312f842761aa2db9f239b006d9e
Gnusson.net <admin@gnusson.net>
2022-03-07 19:42:16 +0100

    New improved splash screen using Suspense and Lazy

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,28 +1,32 @@
-import Header from '../../components/Header/Header';
-import Home from '../Home/Home';
+import React, { Suspense } from 'react';
 import { motion } from 'framer-motion';
 import styles from './App.module.scss';
-import SplashScreen from '../../components/SplashScreen/SplashScreen';
+
+const Header = React.lazy(() => import('../../components/Header/Header'));
+const Home = React.lazy(() => import('../../containers/Home/Home'));
 
 function App() {
   return (
     <div className={styles.app}>
-      <motion.div
-        className={styles.app__splashScreen}
-        initial={{
-          y: '0',
-        }}
-        animate={{
-          y: '-100%',
-        }}
-        transition={{
-          duration: 0.25,
-        }}
-      ></motion.div>
-      <Header />
-      <Home />
+      {/* <Fallback /> */}
+      <Suspense fallback={<Fallback />}>
+        <Header />
+        <Home />
+      </Suspense>
     </div>
   );
 }
 
-export default SplashScreen(App);
+const Fallback = () => {
+  const containerRef = React.useRef();
+  return (
+    <motion.div ref={containerRef} className={styles.app__fallback} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.25, delay: 1 }}>
+      <div className={styles.app__fallback__loader}>
+        <div className={styles.app__fallback__loader__inner}></div>
+      </div>
+      <p className={styles.app__fallback__text}>Loading page...</p>
+    </motion.div>
+  );
+};
+
+export default App;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,10 +4,10 @@ import styles from './Home.module.scss';
 function Home() {
   return (
     <section id="home" className={styles.home}>
-      <motion.h1 initial="hidden" animate="visible" variants={{ visible: { opacity: 1, translateX: 0, transition: { delay: 0.4 } }, hidden: { opacity: 0, translateX: -100 } }}>
+      <motion.h1 initial="hidden" animate="visible" variants={{ visible: { opacity: 1, translateX: 0, transition: { delay: 0.8 } }, hidden: { opacity: 0, translateX: -100 } }}>
         React App!
       </motion.h1>
-      <motion.p initial="hidden" animate="visible" variants={{ visible: { opacity: 1, translateY: 0, transition: { delay: 0.6 } }, hidden: { opacity: 0, translateY: 10 } }}>
+      <motion.p initial="hidden" animate="visible" variants={{ visible: { opacity: 1, translateY: 0, transition: { delay: 1 } }, hidden: { opacity: 0, translateY: 10 } }}>
         Test
       </motion.p>
     </section>
