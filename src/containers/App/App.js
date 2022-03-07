d5b50d5a0ceaf534c923b1b3e8c66f865ca3e1a4
Gnusson.net <admin@gnusson.net>
2022-03-07 15:56:36 +0100

    PWA and loading screen

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,28 @@
 import Header from '../../components/Header/Header';
 import Home from '../Home/Home';
+import { motion } from 'framer-motion';
 import styles from './App.module.scss';
+import SplashScreen from '../../components/SplashScreen/SplashScreen';
 
 function App() {
   return (
     <div className={styles.app}>
+      <motion.div
+        className={styles.app__splashScreen}
+        initial={{
+          y: '0',
+        }}
+        animate={{
+          y: '-100%',
+        }}
+        transition={{
+          duration: 0.25,
+        }}
+      ></motion.div>
       <Header />
       <Home />
     </div>
   );
 }
 
-export default App;
+export default SplashScreen(App);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './scss/main.scss';
 import App from './containers/App/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -9,3 +10,5 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
