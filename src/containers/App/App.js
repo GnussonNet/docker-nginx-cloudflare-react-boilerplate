401899c78219790c9ebb8607ead33868b92916cf
Gnusson.net <admin@gnusson.net>
2022-03-07 20:23:17 +0100

    New improved splash screen

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,32 +1,15 @@
-import React, { Suspense } from 'react';
-import { motion } from 'framer-motion';
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Home from '../../containers/Home/Home';
 import styles from './App.module.scss';
-
-const Header = React.lazy(() => import('../../components/Header/Header'));
-const Home = React.lazy(() => import('../../containers/Home/Home'));
 
 function App() {
   return (
     <div className={styles.app}>
-      {/* <Fallback /> */}
-      <Suspense fallback={<Fallback />}>
-        <Header />
-        <Home />
-      </Suspense>
+      <Header />
+      <Home />
     </div>
   );
 }
-
-const Fallback = () => {
-  const containerRef = React.useRef();
-  return (
-    <motion.div ref={containerRef} className={styles.app__fallback} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.25, delay: 1 }}>
-      <div className={styles.app__fallback__loader}>
-        <div className={styles.app__fallback__loader__inner}></div>
-      </div>
-      <p className={styles.app__fallback__text}>Loading page...</p>
-    </motion.div>
-  );
-};
 
 export default App;
